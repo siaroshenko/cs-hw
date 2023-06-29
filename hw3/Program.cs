b8287a71fe87a4ea985091e22a6a86fe6f8a8765
roshersie <soofiju@gmail.com>
2023-06-29 21:49:54 +0300

    hw3 started

---
+++ hw3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+//21.
+double[] A = new double[3];
+double[] B = new double[3];
+A[0] = Convert.ToDouble(Console.ReadLine());
+A[1] = Convert.ToDouble(Console.ReadLine());
+A[2] = Convert.ToDouble(Console.ReadLine());
+B[0] = Convert.ToDouble(Console.ReadLine());
+B[1] = Convert.ToDouble(Console.ReadLine());
+A[2] = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"{Math.Round(Math.Sqrt(Math.Pow(A[1] - B[1], 2) + Math.Pow(A[0] - B[0], 2) + Math.Pow(A[2] - B[2], 2)), 2)}");
