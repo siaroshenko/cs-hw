bd9ebcba9003d4163077cbaf94058e37957a7f8a
roshersie <soofiju@gmail.com>
2023-07-17 20:50:26 +0300

    bruh

--- hw7/Program.cs
+++ hw7/Program.cs
@@ -14,7 +14,7 @@
         }
         return Convert.ToDouble(inpTemp);
     }
-    
+
     //функция для ввода целого положительного числа
     static int Positive(string text)
     {
@@ -27,7 +27,7 @@
         }
         return Convert.ToInt32(number);
     }
-    
+
     //функция для ввода двумерного массива вещественных чисел
     static double[,] InputReal2DArray(int rows, int col)
     {
@@ -51,11 +51,12 @@
         }
         return arr;
     }
-    
+
     //функция для поиска элемента по индексу в двумерном массиве вещественных чисел
     static double ElementByIndex(double[,] arr, int rowIndex, int colIndex)
     {
-        if (rowIndex >= arr.GetUpperBound(0) + 1 || colIndex >= arr.Length / arr.GetUpperBound(0) + 1) {
+        if (rowIndex >= arr.GetUpperBound(0) + 1 || colIndex >= arr.Length / arr.GetUpperBound(0) + 1)
+        {
             Console.WriteLine($"В массиве нет элемента с индексом [{rowIndex}, {colIndex}].");
             return 0;
         }
@@ -67,7 +68,7 @@
         Console.WriteLine($"Элемент с индексом [{rowIndex}, {colIndex}]: {arr[colIndex, rowIndex]}");
         return arr[colIndex, rowIndex];
     }
-    
+
     //функция для расчёта среднего арифметического в столбце
     static double[] AvgByColumn(double[,] arr)
     {
@@ -92,7 +93,7 @@
         1 -3,3 8 -9,9
         8 7,8 -7,1 9*/
         double[,] arr = InputReal2DArray(Positive("Введите количество строк: "), Positive("Введите количество элементов в строке: "));
-        
+
         /*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
         Например, задан массив:
         1 4 7 2
@@ -100,7 +101,7 @@
         8 4 2 4
         17 -> такого числа в массиве нет*/
         ElementByIndex(arr, Positive("Введите номер строки: ") - 1, Positive("Введите номер столбца: ") - 1);
-        
+
         /*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
         Например, задан массив:
         1 4 7 2
